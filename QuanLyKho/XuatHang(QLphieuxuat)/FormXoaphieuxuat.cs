9c3c88992ee329c5fab6696fab7d7bec506ccba7
admin <your-email=dvtung36@gmail.com>
2020-03-29 20:30:23 +0700

    Add FormMain and start QLPhieuXuat

---
+++ QuanLyKho/XuatHang(QLphieuxuat)/FormXoaphieuxuat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKho.XuatHang_QLphieuxuat_
+{
+    public partial class FormXoaphieuxuat : Form
+    {
+        public FormXoaphieuxuat()
+        {
+            InitializeComponent();
+        }
+    }
+}
